dd25e6afcf50686e82623e68869d679b7d42ab7d
MissCoriel <44127678+MissCoriel@users.noreply.github.com>
2019-03-16 18:21:39 -0500

    Add files via upload

---
+++ ThingstoForget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+//whatever other using stuff. is cool
+namespace EventRepeater
+{
+    public class ThingsToForget
+    {
+        public List<int> RepeatEvents { get; set; }
+        public string Format { get; set; }
+
+        public ThingsToForget()
+        {
+            RepeatEvents = new List<int>();
+            Format = "1.0";
+        }
+    }
+}
