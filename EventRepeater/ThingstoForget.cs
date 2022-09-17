38feca4d9c8a6ce221953b52fbe0ead4fb279083
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2022-09-17 08:37:58 -0500

    Add files via upload

---
+++ EventRepeater/ThingstoForget.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+//whatever other using stuff. is cool
+namespace EventRepeater
+{
+    public class ThingsToForget
+    {
+        public List<int> RepeatEvents { get; set; }
+        public List<string> RepeatMail { get; set; }
+        public List<int> RepeatResponse { get; set; }
+
+        public ThingsToForget()
+        {
+            RepeatEvents = new List<int>();
+            RepeatMail = new List<string>();
+            RepeatResponse = new List<int>();
+            
+        }
+    }
+}
