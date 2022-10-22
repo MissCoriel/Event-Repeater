621f52956b5c0b917e0fa2076d75ad506b921b2c
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2022-10-22 00:58:42 -0500

    Updated  to 6.5.5

---
+++ ConfigModel.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI.Utilities;
+
+namespace EventRepeater
+{
+    internal sealed class ConfigModel
+    {
+        //public SButton EventWindow { get; set; } = SButton.Pause;
+        public KeybindList EmergencySkip { get; set; } = KeybindList.Parse("LeftControl + S");
+        public KeybindList ShowInfo { get; set; } = KeybindList.Parse("LeftControl + I");
+        public KeybindList NormalSkip { get; set; } = KeybindList.Parse("LeftAlt + S");
+
+    }
+}
